5b0bb411f8e2187a62d4cd42138cea6e4ee09fe1
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-09-18 22:16:57 +0200

    deployment config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
   url: "https://rentoken-xyz.github.io",
-  baseUrl: "/",
+  baseUrl: "/rent-sell-nft-marketplace/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
